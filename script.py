9b35a319d38234d2c5c381795acde10038c464d2
Ankur Gupta <rukna1000@gmail.com>
2018-09-03 11:36:23 -0400

    some issues with the math, going to switch over to math based on prime merdian instead

--- script.py
+++ script.py
@@ -11,6 +11,9 @@ def lambda_handler(event, context):
 
 
 def get_location_for_time(time):
+    wpi_lat = 42.2746
+    wpi_long = 71.8063
+
     current_date = datetime.datetime.now()
 
     if is_hour_forward(current_date):
@@ -21,13 +24,22 @@ def get_location_for_time(time):
     days_delta = datetime.date(current_date.year, 1, 1) - current_date.date()
     days_delta = abs(days_delta.days)
 
-    print (days_delta)
     B = radians((days_delta - 81) * 360 / 365)
-    print("b ", B)
     E = 9.87 * sin(2 * B) - 7.53 * cos(B) - 1.58 * sin(B)
-    print("e ", E)
-    time_correction = 4 * (delta * 15 + 71.8063) + E
-    return time_correction
+    time_correction = 4 * (delta * 15 + wpi_lat) + E
+
+    adjusted_time = current_date + datetime.timedelta(minutes=time_correction)
+    difference = time - adjusted_time
+
+    print(adjusted_time)
+    print(difference)
+
+    lon_change = (difference.days * 24 * 60 * 60 + difference.seconds) * \
+            15 / 60 ** 2
+    
+    print(lon_change)
+    adjusted_lon = -wpi_long + lon_change
+    return adjusted_lon
 
 def is_hour_forward(t):
     if t.month < 3 or t.month > 11:
@@ -41,4 +53,11 @@ def is_hour_forward(t):
         first_sunday = (7 - datetime.datetime(t.year, 11, 1).weekday()) % 7
         return t.day <= first_sunday
 
-print(get_location_for_time("val"))
+current_date = datetime.datetime.now()
+print(get_location_for_time(datetime.datetime(
+    current_date.year,
+    current_date.month,
+    current_date.day,
+    17,
+    0,
+    0)))
